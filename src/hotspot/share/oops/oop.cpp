f367b2d2608f7d548609009e0687a1c61f0263c4
Dat Nguyen <tiendat.ng.cs@gmail.com>
2022-03-21 14:08:40 -0500

    check for valid dummy

--- src/hotspot/share/oops/oop.cpp
+++ src/hotspot/share/oops/oop.cpp
@@ -231,181 +231,181 @@ oop oopDesc::oop_or_null(address addr) {
 oop oopDesc::obj_field_acquire(int offset) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::oop_load_at(as_oop(), offset);
 }
 
 void oopDesc::obj_field_put_raw(int offset, oop value) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   RawAccess<>::oop_store_at(as_oop(), offset, value);
 }
 void oopDesc::release_obj_field_put(int offset, oop value) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::oop_store_at(as_oop(), offset, value);
 }
 void oopDesc::obj_field_put_volatile(int offset, oop value) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_SEQ_CST>::oop_store_at(as_oop(), offset, value);
 }
 
 address oopDesc::address_field(int offset) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<>::load_at(as_oop(), offset);
 }
 address oopDesc::address_field_acquire(int offset) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 
 void oopDesc::address_field_put(int offset, address value) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<>::store_at(as_oop(), offset, value);
 }
 void oopDesc::release_address_field_put(int offset, address value)    {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 Metadata* oopDesc::metadata_field(int offset) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<>::load_at(as_oop(), offset);
 }
 void oopDesc::metadata_field_put(int offset, Metadata* value) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<>::store_at(as_oop(), offset, value);
 }
 
 Metadata* oopDesc::metadata_field_acquire(int offset) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_metadata_field_put(int offset, Metadata* value) {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 jbyte oopDesc::byte_field_acquire(int offset)                   {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_byte_field_put(int offset, jbyte value)         {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 jchar oopDesc::char_field_acquire(int offset)                   {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_char_field_put(int offset, jchar value)         {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 jboolean oopDesc::bool_field_acquire(int offset)                {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_bool_field_put(int offset, jboolean value)      {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, jboolean(value & 1));
 }
 
 jint oopDesc::int_field_acquire(int offset)                     {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_int_field_put(int offset, jint value)           {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 jshort oopDesc::short_field_acquire(int offset)                 {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_short_field_put(int offset, jshort value)       {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 jlong oopDesc::long_field_acquire(int offset)                   {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_long_field_put(int offset, jlong value)         {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 jfloat oopDesc::float_field_acquire(int offset)                 {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_float_field_put(int offset, jfloat value)       {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
 
 jdouble oopDesc::double_field_acquire(int offset)               {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   return HeapAccess<MO_ACQUIRE>::load_at(as_oop(), offset);
 }
 void oopDesc::release_double_field_put(int offset, jdouble value)     {
   // tty->print_cr("Increasing ac here");
   // _access_counter+=1;
-  increase_access_counter();
+  // increase_access_counter();
   HeapAccess<MO_RELEASE>::store_at(as_oop(), offset, value);
 }
