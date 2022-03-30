0cb85965b01c334058e5c0950281a574863d23c9
Dat Nguyen <tiendat.ng.cs@gmail.com>
2022-03-29 20:37:35 -0500

    try macro asm

--- src/hotspot/cpu/x86/c1_LIRAssembler_x86.cpp
+++ src/hotspot/cpu/x86/c1_LIRAssembler_x86.cpp
@@ -1618,19 +1618,19 @@ void LIR_Assembler::emit_increase_access_counter(LIR_OpIncrAC* op) {
   if (UseCompressedOops) {
     __ decode_heap_oop(base_oop);
   }
-  // // __ pusha();
-  // // __ call_VM_leaf(CAST_FROM_FN_PTR(address, InterpreterRuntime::print_oop), base_oop);
-  // // __ popa();
+  // __ pusha();
+  // __ call_VM_leaf(CAST_FROM_FN_PTR(address, InterpreterRuntime::print_oop), base_oop);
+  // __ popa();
 
-  // // load base_oop gc_epoch to tmp1
+  // load base_oop gc_epoch to tmp1
 
-  // // cmp tmp1 to static_gc_epoch if equal jmp to no_reset_values, 
-  // __ movptr(tmp1, InternalAddress((address)(&oopDesc::static_gc_epoch)));
-  // // __ pusha();
-  // // __ call_VM_leaf(CAST_FROM_FN_PTR(address, ShenandoahRuntime::print_address), tmp1);
-  // // __ popa();
+  // cmp tmp1 to static_gc_epoch if equal jmp to no_reset_values, 
+  __ movptr(tmp1, InternalAddress((address)(&oopDesc::static_gc_epoch)));
+  // __ pusha();
+  // __ call_VM_leaf(CAST_FROM_FN_PTR(address, ShenandoahRuntime::print_address), tmp1);
+  // __ popa();
   
-  // __ cmpptr(tmp1, Address(base_oop, oopDesc::gc_epoch_offset_in_bytes()));
+  __ cmpptr(tmp1, Address(base_oop, oopDesc::gc_epoch_offset_in_bytes()));
   // __ jcc(Assembler::equal, no_reset_values);
   // // Reset ac to 0 and gc_epoch to current gc_epoch
   // __ movptr(Address(base_oop, oopDesc::gc_epoch_offset_in_bytes()), tmp1);
@@ -1649,13 +1649,13 @@ void LIR_Assembler::emit_increase_access_counter(LIR_OpIncrAC* op) {
   // __ movptr(Address(base_oop, oopDesc::access_counter_offset_in_bytes()), tmp1);
 
 
-  // // __ pusha();
-  // // __ call_VM_leaf(CAST_FROM_FN_PTR(address, InterpreterRuntime::print_oop), base_oop);
-  // // __ popa();
+  // __ pusha();
+  // __ call_VM_leaf(CAST_FROM_FN_PTR(address, InterpreterRuntime::print_oop), base_oop);
+  // __ popa();
 
-  // // __ pusha();
-  // // __ call_VM_leaf(CAST_FROM_FN_PTR(address, InterpreterRuntime::print_newline));
-  // // __ popa();
+  // __ pusha();
+  // __ call_VM_leaf(CAST_FROM_FN_PTR(address, InterpreterRuntime::print_newline));
+  // __ popa();
   if (UseCompressedOops) {
     __ encode_heap_oop(base_oop);
   }
